0355f61e297f1488e9836a935bb0c60eead7f993
Oriol <bover73@gmail.com>
2019-10-18 10:51:06 +0200

    add products container and fetch logic

---
+++ src/containers/ProductsContainer.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { ListOfProducts } from '../components/ListOfProducts'
+
+export const ProductsContainer = () => {
+  const [loading, setLoading] = useState(false)
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true)
+        const { data } = await axios.get('https://api.myjson.com/bins/en2ik')
+        console.log(data)
+        setProducts(data)
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    getProducts()
+  }, [])
+
+  return (
+    <>
+      {loading ? <h2>Loading..</h2> : <ListOfProducts products={products} />}
+    </>
+
+  )
+}
